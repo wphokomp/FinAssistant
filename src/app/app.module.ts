7770ba06daf17cccab4f4891ae7600c7593f88eb
Doctor Bokisi <Doctor.Bokisi@ssp-worldwide.com>
2018-09-29 19:24:54 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
