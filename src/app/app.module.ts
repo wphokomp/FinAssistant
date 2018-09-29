90eb39b24c1e14d5a88533d87c9da2714a589223
lsmucassi <lindasmucassi@gmail.com>
2018-09-29 22:43:36 +0200

    Do: paymets component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { StatsComponent } from './stats/stats.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    StatsComponent
+    StatsComponent,
+    PaymentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/payments/payments.component.spec.ts
@@ -0,0 +1,25 @@
+
+import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaymentsComponent } from './payments.component';
+
+
+describe('PaymentsComponent', () => {
+  let component: PaymentsComponent;
+  let fixture: ComponentFixture<PaymentsComponent>;
+
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PaymentsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PaymentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
